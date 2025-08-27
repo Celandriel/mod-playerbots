ad1ba22094a5631f0a4459862a2ff27e65488e05
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-08-27 15:31:26 -0700

    Rename config option

--- src/PlayerbotAIConfig.cpp
+++ src/PlayerbotAIConfig.cpp
@@ -218,7 +218,7 @@ bool PlayerbotAIConfig::Initialize()
         restrictedHealerDPSMaps);
 	
     //////////////////////////// Guild RPG
-    enableGuildRpgStrategy = sConfigMgr->GetOption<bool>("AiPlayerbot.Enable_Guild_RPG", true);   
+    enableGuildRpgStrategy = sConfigMgr->GetOption<bool>("AiPlayerbot.EnableGuildRPG", true);   
     LoadList<std::vector<uint32>>(sConfigMgr->GetOption<std::string>("AiPlayerbot.GuildTypeRatios", "40,20,20,10,10"), GuildTypeRatios);
     LoadList<std::vector<uint32>>(sConfigMgr->GetOption<std::string>("AiPlayerbot.GuildSize", "4,3,2,2,2"), GuildSize);
     LoadList<std::vector<uint32>>(sConfigMgr->GetOption<std::string>("AiPlayerbot.GuildPVPSpecRatio", "0,33,33,33"), GuildPVPSpecRatio);
